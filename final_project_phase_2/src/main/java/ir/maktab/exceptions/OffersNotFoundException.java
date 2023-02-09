9373e790e3f01e98030e111ca871c4395736bcc9
phylix <alirezarastegar117390@gmail.com>
2023-02-09 15:04:36 +0330

    34 --> Creating OffersNotFoundException class for handling offer not found exception.

---
+++ final_project_phase_2/src/main/java/ir/maktab/exceptions/OffersNotFoundException.java
@@ -0,0 +1,8 @@
+package ir.maktab.exceptions;
+
+public class OffersNotFoundException extends RuntimeException {
+
+    public OffersNotFoundException(String message) {
+        super(message);
+    }
+}
