7ddb040ce752d3d64084b9c5d7ab7aa65165cb6c
phylix <alirezarastegar117390@gmail.com>
2023-02-09 14:00:02 +0330

    7 --> create admin class as entity

---
+++ final_project_phase_2/src/main/java/ir/maktab/entity/Admin.java
@@ -0,0 +1,22 @@
+package ir.maktab.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class Admin extends BaseEntity<Long> {
+
+    String username;
+    Integer age;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    User user;
+}
