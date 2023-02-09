4ea069f2bf8434747fa2c5ac3111c0e7f63def88
phylix <alirezarastegar117390@gmail.com>
2023-02-09 14:02:50 +0330

    9 --> Creating a user class to create a table of users in the database.

---
+++ final_project_phase_2/src/main/java/ir/maktab/entity/User.java
@@ -0,0 +1,32 @@
+package ir.maktab.entity;
+
+
+import ir.maktab.entity.enumeration.UserType;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "Users")
+public class User extends BaseEntity<Long> {
+
+    String firstname;
+    String lastname;
+    String email;
+    String password;
+
+    @CreationTimestamp
+    LocalDateTime registerDate;
+
+    @Enumerated(EnumType.STRING)
+    UserType userType;
+}
