0371aca79c762714e8bca5c4023497763f41fd01
phylix <alirezarastegar117390@gmail.com>
2023-02-09 15:08:05 +0330

    36 --> Creating PasswordNotMatchException class to handle password mismatch by repeating it exception.

---
+++ final_project_phase_2/src/main/java/ir/maktab/exceptions/PasswordNotMatchException.java
@@ -0,0 +1,8 @@
+package ir.maktab.exceptions;
+
+public class PasswordNotMatchException extends RuntimeException {
+
+    public PasswordNotMatchException(String message) {
+        super(message);
+    }
+}
