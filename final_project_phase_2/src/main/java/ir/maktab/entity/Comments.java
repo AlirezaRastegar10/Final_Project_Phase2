e4a062f18c31aff12f6ad2c105317d2a4acd872b
phylix <alirezarastegar117390@gmail.com>
2023-02-09 14:00:54 +0330

    8 --> create Comments class as entity

---
+++ final_project_phase_2/src/main/java/ir/maktab/entity/Comments.java
@@ -0,0 +1,25 @@
+package ir.maktab.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class Comments extends BaseEntity<Long> {
+
+    @ManyToOne
+    Customer customer;
+
+    @ManyToOne
+    Expert expert;
+
+    Integer score;
+    String content;
+}
