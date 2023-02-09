59624ca30ad98d0af6ad9defad207e996e96f6f4
phylix <alirezarastegar117390@gmail.com>
2023-02-09 14:06:14 +0330

    13 --> Creating an Offers class to store expert suggestions in the database.

---
+++ final_project_phase_2/src/main/java/ir/maktab/entity/Offers.java
@@ -0,0 +1,35 @@
+package ir.maktab.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class Offers extends BaseEntity<Long> {
+
+    @ManyToOne
+    Expert expert;
+
+    @CreationTimestamp
+    LocalDateTime registerDateAndTime;
+
+    Long proposedPrice;
+    LocalDateTime suggestedTime;
+    String durationOfWork;
+
+    @ManyToMany(mappedBy = "offersSet", fetch = FetchType.EAGER)
+    Set<Orders> ordersSet = new HashSet<>();
+}
