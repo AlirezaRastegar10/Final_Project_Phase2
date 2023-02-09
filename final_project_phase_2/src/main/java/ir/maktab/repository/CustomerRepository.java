f2cc5999fdc35bc9c39b9a0d30d6dd4c90dd4bf6
phylix <alirezarastegar117390@gmail.com>
2023-02-09 14:19:22 +0330

    19 --> Creating the CustomerRepository interface to communicate with the customer table.

---
+++ final_project_phase_2/src/main/java/ir/maktab/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package ir.maktab.repository;
+
+import ir.maktab.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
