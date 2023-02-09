2a9827683cdac8c43ccfc279c849f02f46f88d4d
phylix <alirezarastegar117390@gmail.com>
2023-02-09 14:52:40 +0330

    23 --> Creating the OrdersRepository interface to communicate with the orders table.

---
+++ final_project_phase_2/src/main/java/ir/maktab/repository/OrdersRepository.java
@@ -0,0 +1,22 @@
+package ir.maktab.repository;
+
+
+import ir.maktab.entity.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
+
+    @Query("select o from Orders o where o.underDuty.id= ?1 and " +
+            "(o.orderStatus='WAITING_FOR_THE_SUGGESTION_OF_EXPERTS' or o.orderStatus='WAITING_FOR_SPECIALIST_SELECTION')")
+    List<Orders> findAllByUnderDutyIdAndOrderStatus(Long id);
+
+    @Query("select o from Orders o where o.underDuty.id= ?1 and o.id= ?2 and " +
+            "(o.orderStatus='WAITING_FOR_THE_SUGGESTION_OF_EXPERTS' or o.orderStatus='WAITING_FOR_SPECIALIST_SELECTION')")
+    Orders findByUnderDutyIdAndOrderStatus(Long underDutyId, Long orderId);
+}
