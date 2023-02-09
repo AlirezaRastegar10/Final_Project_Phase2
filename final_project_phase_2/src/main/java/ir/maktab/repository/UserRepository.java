60667a4075ba991ceba320c8b7b1d50617381ac8
phylix <alirezarastegar117390@gmail.com>
2023-02-09 14:17:20 +0330

    18 --> Creating the UserRepository interface to communicate with the users table.

---
+++ final_project_phase_2/src/main/java/ir/maktab/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package ir.maktab.repository;
+
+import ir.maktab.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Boolean existsByEmail(String email);
+
+    User findByEmail(String email);
+    User findByEmailAndId (String email, Long id);
+}
